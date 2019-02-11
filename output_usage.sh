89c4003ad9aab64076f24d9245a266c1ae9a3348
Greg Guthe <gguthe@mozilla.com>
2019-02-11 12:59:09 -0500

    add script for generating usage output for the readme

---
+++ output_usage.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+## outputs the usage section for the audit-filter README to readme_usage.md
+
+cat > readme_usage.md <<EOF
+\$ audit-filter -h
+$(audit-filter -h)
+\$ cd audit-filter/example/
+\$ cat package.json
+$(cat example/package.json)
+\$ npm --version
+$(npm --version)
+\$ npm audit
+$(cd example/ && npm audit)
+\$ echo \$?
+$(echo $?)
+\$ cat .nsprc
+$(cat example/.nsprc)
+\$ npm audit --json | audit-filter
+$(cd example/ && npm audit --json | audit-filter)
+\$ echo \$?
+$(echo $?)
+\$ # Alternatively specify audit and config file paths (note: errors print to stderr)
+\$ cd .. && audit-filter --nsp-config tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-nsprc.json --audit tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-npm-6.4.1-audit.json
+$(audit-filter --nsp-config tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-nsprc.json --audit tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-npm-6.4.1-audit.json 2>&1)
+\$ echo \$?
+$(echo $?)
+\$ # use --json for JSON output
+\$ audit-filter --json --nsp-config tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-nsprc.json --audit tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-npm-6.4.1-audit.json
+$(audit-filter --json --nsp-config tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-nsprc.json --audit tests/fixtures/screenshots-0191b17d3bac5de51efa7acbaa0d52bb26c91573-npm-6.4.1-audit.json 2>&1)
+EOF
